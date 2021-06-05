7c02a225b6c35978414c4d0720efa5c2783d37af
kordian ptak <kordianbird@Gmail.com>
2021-06-05 21:34:13 +0000

    added div with class container to index.html, added card and container css along with grid system to style.css, created eventListener that generates divs in script.js

--- js/script.js
+++ js/script.js
@@ -4,9 +4,15 @@ function startGame() {
 
     if (hide) {
         for ( var x = 0; x < hide.length; x++) {
-            hide[x].style.visibility = "hidden";
+            hide[x].style.display = "none";
         }
     }
 }
 
 document.getElementById("game").addEventListener('click', startGame);
+
+document.getElementById("game").addEventListener('click', function() {
+    for(x = 0; x < 9; x++) {
+        $('<div class="card col-4"></div>').appendTo('.container');
+    }
+}, false);
