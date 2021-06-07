1d540289529c25c8125d2c8ee24c9e87357108b5
kordian ptak <kordianbird@Gmail.com>
2021-06-07 11:30:51 +0000

    deleted eventListener(function) and instead implemented the divs in index.html, shortened startGame function and created unhide function, changed css for button element and deleted grid system in style.css

--- js/script.js
+++ js/script.js
@@ -1,18 +1,13 @@
 
 function startGame() {
-    var hide = document.getElementsByClassName("hide");
-
-    if (hide) {
-        for ( var x = 0; x < hide.length; x++) {
-            hide[x].style.display = "none";
-        }
-    }
+  $(".hide").hide();
 }
 
-document.getElementById("game").addEventListener('click', startGame);
 
-document.getElementById("game").addEventListener('click', function() {
-    for(x = 0; x < 9; x++) {
-        $('<div class="card"></div>').appendTo('.container');
+
+function unhide() {
+    var T = $(".unhide");
+    for (var i = 0; i < T.length; i++) {
+        T[i].style.display = 'block';
     }
-}, false);
+}
