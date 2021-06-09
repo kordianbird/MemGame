ba544769e9cbca7b5699c0d4e3ed4e46e4277145
kordian ptak <kordianbird@Gmail.com>
2021-06-09 18:52:35 +0000

    added another if statement into function playerTurn()

--- js/script.js
+++ js/script.js
@@ -51,6 +51,12 @@ function playerTurn(tile) {
     const index = playerSequence.push(tile) - 1;
 
     if (playerSequence.length = sequence.length) {
-        alert("damn... looks like you lost")
+        alert("damn... looks like you lost");
+    }
+
+    if (playerSequence.length === sequence.length) {
+        if (playerSequence.length === 10) {
+            alert("damn you're good!");
+        }
     }
 }
